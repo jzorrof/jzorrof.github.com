4a6ec92012aec6c8755e8f4515916ed467d1c541
fanzhong <zhongfan@lewatek.com>
2015-05-14 19:17:10 +0800

    modify formpost.sh

--- formpost.sh
+++ formpost.sh
@@ -3,12 +3,11 @@
 # 日期：2015-05-07
 #
 
-POSTPATH=/Users/joe_fan/Development/jzorrof.github.com/_posts
+POSTPATH=$PWD/_posts
 DATE=`date "+%Y-%m-%d"`
 POSTNAME=`echo $POSTPATH/$DATE-$1.md`
 
-
-if [[ -e $POSTNAME ]] && [[ $1=='' ]]; then
+if [[ -e $POSTNAME ]] || [[ $1=='' ]]; then
 	echo "exist"
 else
 	touch $POSTPATH/$DATE-$1.md
